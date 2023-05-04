bd556276a9b0f32cff9a4e20897c78ed34a07b5a
Doker13 <Doker13@github.com>
2023-05-04 05:24:40 -0400

    Change main

--- second/src/main.c
+++ second/src/main.c
@@ -4,7 +4,7 @@
 #include "IntVector.h"
 
 void print_vector(intvector *v) {
-  printf("size=%ld cap=%ld\nEl\n", int_vector_get_size(v),
+  printf("Size=%ld Capacity=%ld\nElements:\n", int_vector_get_size(v),
          int_vector_get_capacity(v));
   for (int i = 0; i < int_vector_get_capacity(v); i++) {
     printf("%d ", int_vector_get_item(v, i));
@@ -13,21 +13,44 @@ void print_vector(intvector *v) {
 }
 
 int main() {
-  intvector *v = int_vector_new(8);
-  for (int i = 0; i < 20; i++) {
+  intvector *v = int_vector_new(10);
+  printf("Создаём вектор v с ёмкостью 10:\n");
+  print_vector(v);
+  
+  for (int i = 0; i < 10; i++) {
     int_vector_set_item(v, i, i);
   }
+  printf("Заполняем вектор v, размер теперь также равен 10:\n");
   print_vector(v);
-
+  
   intvector *t = int_vector_copy(v);
-  print_vector(t);
-  int_vector_free(t);
-  
-  printf("%d\n", int_vector_get_item(v, 4));
-  
-  int_vector_push_back(v, 11);
-  print_vector(v);
   int_vector_free(v);
+  printf("Копируем вектор v в вектор t и освобождаем память для вектора v:\n");
+  print_vector(t);
+  
+  printf("Пятый элемент вектора t: %d\n\n", int_vector_get_item(t, 4));
+  
+  printf("Добавляем один элемент в конец заполненного вектора t:\n");
+  int_vector_push_back(t, 10);
+  print_vector(t);
+  
+  printf("Удаляем последний элемент вектора t:\n");
+  int_vector_pop_back(t);
+  print_vector(t);
+  
+  printf("Увеличиваем ёмкость вектора t до 15:\n");
+  int_vector_reserve(t, 15);
+  print_vector(t);
+  
+  printf("Увеличиваем размер вектора t до 13:\n");
+  int_vector_resize(t, 13);
+  print_vector(t);
+
+  printf("Уменьшаем ёмкость вектора t до его размера, после чего очищаем память для вектора t:\n");
+  int_vector_shrink_to_fit(t);
+  print_vector(t);
+  
+  int_vector_free(t);
 
   return 0;
 }

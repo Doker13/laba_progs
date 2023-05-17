89692e4df5711af07b2e5bc92901c22d13be9f1f
Doker13 <Doker13@github.com>
2023-05-17 01:39:53 -0400

    Dlete commits

--- third/ex_second/src/coder.c
+++ third/ex_second/src/coder.c
@@ -5,19 +5,19 @@
 #include "command.h"
 
 int encode(uint32_t code_point, CodeUnits *code_units) {
-  if (code_point < 0x80) { //<8
+  if (code_point < 0x80) {
     code_units->length = 1;
     code_units->code[0] = code_point;
-  } else if (code_point < 0x800) { //<12
+  } else if (code_point < 0x800) {
     code_units->length = 2;
     code_units->code[0] = 0xc0 | (code_point >> 6);
-    code_units->code[1] = 0x80 | (code_point & 0x3f); //00111111
-  } else if (code_point < 0x10000) { //<17
+    code_units->code[1] = 0x80 | (code_point & 0x3f);
+  } else if (code_point < 0x10000) {
     code_units->length = 3;
     code_units->code[0] = 0xe0 | (code_point >> 12);
     code_units->code[1] = 0x80 | ((code_point >> 6) & 0x3f);
     code_units->code[2] = 0x80 | (code_point & 0x3f);
-  } else if (code_point < 0x200000) { //<22
+  } else if (code_point < 0x200000) {
     code_units->length = 4;
     code_units->code[0] = 0xf0 | (code_point >> 18);
     code_units->code[1] = 0x80 | ((code_point >> 12) & 0x3f);
@@ -33,15 +33,15 @@ int encode(uint32_t code_point, CodeUnits *code_units) {
 uint32_t decode(const CodeUnits *code_unit) {
   if (code_unit->code[0] < 0x80) {
     return code_unit->code[0];
-  } else if (code_unit->code[0] < 0xE0) { //<11100000
-    return ((code_unit->code[0] & 0x1f) << 6) | (code_unit->code[1] & 0x3f); //00111111
-  } else if (code_unit->code[0] < 0xF0) { //<11110000
-    return ((code_unit->code[0] & 0x0f) << 12) | //00001111
-           ((code_unit->code[1] & 0x3f) << 6) | (code_unit->code[2] & 0x3f); //00111111
-  } else if (code_unit->code[0] < 0xF8) { //<11111000
-    return ((code_unit->code[0] & 0x07) << 18) | //00000111
-           ((code_unit->code[1] & 0x3f) << 12) | //00111111
-           ((code_unit->code[2] & 0x3f) << 6) | (code_unit->code[3] & 0x3f); //00111111
+  } else if (code_unit->code[0] < 0xE0) {
+    return ((code_unit->code[0] & 0x1f) << 6) | (code_unit->code[1] & 0x3f);
+  } else if (code_unit->code[0] < 0xF0) {
+    return ((code_unit->code[0] & 0x0f) << 12) |
+           ((code_unit->code[1] & 0x3f) << 6) | (code_unit->code[2] & 0x3f);
+  } else if (code_unit->code[0] < 0xF8) {
+    return ((code_unit->code[0] & 0x07) << 18) |
+           ((code_unit->code[1] & 0x3f) << 12) |
+           ((code_unit->code[2] & 0x3f) << 6) | (code_unit->code[3] & 0x3f);
   } else {
     printf("Не удалось декодировать\n");
     return -1;
@@ -53,10 +53,10 @@ int read_next_code_unit(FILE *in, CodeUnits *code_units) {
   if (code_units->code[0] < 0x80) {
     code_units->length = 1;
     return 0;
-  } else if (code_units->code[0] < 0xE0) { //11100000
+  } else if (code_units->code[0] < 0xE0) {
     code_units->length = 2;
     fread(&(code_units->code[1]), 1, 1, in);
-    if (code_units->code[1] >= 0x80 && code_units->code[1] <= 0xBF) { // в отрезке от 10000000 до 10111111
+    if (code_units->code[1] >= 0x80 && code_units->code[1] <= 0xBF) {
       return 0;
     } else {
       return -1;
---
+++ third/test.c
@@ -0,0 +1,58 @@
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+
+size_t encode_varint(uint32_t value, uint8_t* buf)
+{
+    assert(buf != NULL);
+    uint8_t* cur = buf;
+    while (value >= 0x80) {
+        const uint8_t byte = (value & 0x7f) | 0x80;
+        *cur = byte;
+        value >>= 7;
+        ++cur;
+    }
+    *cur = value;
+    ++cur;
+    return cur - buf;
+}
+
+uint32_t decode_varint(const uint8_t** bufp)
+{
+    const uint8_t* cur = *bufp;
+    uint8_t byte = *cur++;
+    uint32_t value = byte & 0x7f;
+    size_t shift = 7;
+    while (byte >= 0x80) {
+        byte = *cur++;
+        value += (byte & 0x7f) << shift;
+        shift += 7;
+    }
+    *bufp = cur;
+    return value;
+}
+
+int main(){
+    FILE *compresseds;
+    compresseds = fopen("compressed.dat", "wb");
+    uint32_t value = 0x3333a;
+    uint8_t buf[4];
+    size_t size=0;
+    uint8_t comressed[100];
+    uint8_t *curent=comressed;
+    uint8_t *start = curent;
+    size=encode_varint(value,buf);
+    for (int j = 0; j < size; j++){
+    	*curent = buf[j];
+     	curent++;
+    }
+    for(int i=0; i < size; i++){
+	printf("%x", buf[i]);
+    }
+    fwrite(start, sizeof(uint8_t), curent - start, compresseds);
+    printf("\n");
+    const uint8_t *cur_comp = comressed;
+    printf("%x\n",decode_varint(&cur_comp));
+    return 0;
+}
